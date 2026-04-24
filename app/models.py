e7ed7028222189bbffcb6c20b3c40b7dc0d67881
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-24 18:29:01 +1000

    Fixed the relationship between the User and posts model in the database

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -19,7 +20,7 @@ class Post(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    author: so.Mapped[str] = so.relationship(back_populates='posts')
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
     
     def __repr__(self):
-        return '<Post {}>'.form(self.body)
+        return '<Post {}>'.format(self.body)
