1970b43e06f7b007dc87fece51c5e7eedc1a750b
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-25 15:56:38 +1000

    Added requiring to login function and redirect back to main viewing page

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import loginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
+from urllib.parase import urlsplit
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Rithy'}
     posts = [
@@ -33,6 +35,9 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for(index)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
