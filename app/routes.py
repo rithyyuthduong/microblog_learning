cee8ac40a7ce7ee00b21802c9c77fcf299fed07e
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-25 15:49:08 +1000

    Added logout function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import loginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app.models import User
 
@@ -35,3 +35,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
