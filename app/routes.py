08bedc7af3a6c94e360809d644bf483c08e4b3f8
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-25 22:00:11 +1000

    Added record for the last seen on for a user

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
 
 
 @app.route('/')
@@ -68,3 +69,9 @@ def user(username):
         {'author': user, 'body': 'Test 2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
