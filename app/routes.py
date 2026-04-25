a9e23c4e4b7a71034c24ffa523c5f3480f8e5d12
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-25 15:44:34 +1000

    Added login view function

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,13 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import loginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
-@app.route('/login', methods=['GET', 'POST'])
-
-def login():
-    form = loginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for("index"))
-    return render_template('login.html', title='Sign In', form=form)
-
 def index():
     user = {'username': 'Rithy'}
     posts = [
@@ -28,3 +21,17 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = loginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
