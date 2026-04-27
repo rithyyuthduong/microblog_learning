2302a122c50cef011d0417c7d9a959e508f06c43
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-27 15:14:26 +1000

    added filtering for posts followers and users

--- app/models.py
+++ app/models.py
@@ -57,6 +57,20 @@ class User(UserMixin, db.Model):  ## The database table
         query = sa.select(sa.func.count()).select_from(
             self.followers.select().subquery())
         return db.session.scalar(query)
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower), isouter=True)
+            .where(sa.or_(
+                Follower.id == self.id,
+                Author.id == self.id,
+            ))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())
+        )
 
 class Post(db.Model):     ##Post values
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
