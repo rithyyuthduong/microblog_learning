ddd4f6b93f5f8d61eb24ffe16e9bd90b6d522d94
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-30 17:32:19 +1000

    added password reset token

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 followers = sa.Table(
     'followers',
@@ -77,6 +79,19 @@ class User(UserMixin, db.Model):  ## The database table
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256'
+        )
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KET'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
 class Post(db.Model):     ##Post values
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
