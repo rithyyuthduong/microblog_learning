4ee2ed3c3acc155e7ba013faa3fdc8c995395e20
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-30 16:53:51 +1000

    added pagination links in the user profile template

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,11 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = db.paginate(current_user.following_posts(), page=page,
                         per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Home page', posts=posts)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home page', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST']) # Login method. Checks if the user is authenticated. If not will be routed to login form. Checks to see if User is in the database or not. If invalid it will flash an error message. When logged in, will remember user data and send out a request to send the user back to the page the user was trying to go back to.
 def login():
