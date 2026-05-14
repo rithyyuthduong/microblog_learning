d05c92c054f0f7f641e748814ae5c3c7842c7bbf
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-05-14 15:59:18 +1000

    Fixed: Typo in notifications model.

--- app/models.py
+++ app/models.py
@@ -232,7 +232,7 @@ class Notification(db.Model):
     timestamp: so.Mapped[float] = so.mapped_column(index=True, default=time)
     payload_json: so.Mapped[str] = so.mapped_column(sa.Text)
     
-    user: so.Mapped[User] = so.relationship(back_populates='notification')
+    user: so.Mapped[User] = so.relationship(back_populates='notifications')
     
     def get_data(self):
         return json.loads(str(self.payload_json))
